da80685fc622f53cc36909befb79787ee9e44e9a
yonkeenn <jhon.vargasr@gmail.com>
2022-11-21 10:41:37 -0500

    Updating server

--- App/server.js
+++ App/server.js
@@ -1,6 +1,18 @@
-const name = 'Jhon Vargas'
-const greeting = function(){
-    console.log(`Hello ${name}, welcome to NodeJS`)
-}
+const { read } = require('fs')
+const http = require('http')
 
-greeting(name)
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/'){
+        res.end('<h1>Home Page </h1>')
+    } else if (req.url === '/about'){
+        res.end('<h1>About Page </h1>')
+    } else {
+        res.end('Page not found')
+    }
+    
+})
+
+server.listen(5000, 'localhost', () => {
+    console.log('Server is listening at localhost on port 5000')
+})
